1a3bf1dbc1a92bdca35947b2edfa0ca9b5120493
Casper <casper@rogers-MacBook-Pro.local>
2018-03-29 15:21:50 +0200

    INIT

---
+++ app/Todo.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Todo extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'done'
+    ];
+}
